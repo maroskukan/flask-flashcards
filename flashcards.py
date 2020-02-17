1e597b5dfd8ede11d4ba58bc8b3c5714f6d044c3
closevision <maros.kukan@me.com>
2020-02-17 11:39:12 +0100

    Page counter added.

--- flashcards.py
+++ flashcards.py
@@ -1,15 +1,23 @@
 from datetime import datetime
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # Creates Flask object with name of the module
 
 
-@app.route("/")
-def welcome():
+@app.route("/") # Decorator maps URL to a function defined below
+def welcome(): 
     return "Welcome to my Flash Cards application!"
 
-# Page the counts number of visits
+# In backgound, such mapping is added to special attribute app.url_map
 
 @app.route("/date")
 def date():
     return "This page was served at " + str(datetime.now())
+
+counter = 0
+
+@app.route("/count_views")
+def count_demo():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times"

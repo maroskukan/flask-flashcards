fd3f96c009ed871e7dee66a47bcd1108a4a8b22e
closevision <maros.kukan@me.com>
2020-02-17 16:10:34 +0100

    Cards view modified.

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 from model import db
 
@@ -14,10 +14,13 @@ def welcome():
     )
 
 
-@app.route("/card")
-def card_view():
-    card = db[0]
-    return render_template(
-        "card.html",
-        card=card
-    )
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template(
+            "card.html",
+            card=card
+        )
+    except IndexError:
+        abort(404)

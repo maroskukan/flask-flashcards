fd2f2a2d74d4eccd3c469f3594373166ffc5da53
closevision <maros.kukan@me.com>
2020-02-17 15:34:32 +0100

    Data model added.

--- flashcards.py
+++ flashcards.py
@@ -1,23 +1,23 @@
-from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template
+
+from model import db
+
 
 app = Flask(__name__) # Creates Flask object with name of the module
 
 
 @app.route("/") # Decorator maps URL to a function defined below
-def welcome(): 
-    return "Welcome to my Flash Cards application!"
+def welcome():
+    return render_template(
+        "welcome.html",
+        message="Here's a message from the view."
+    )
 
-# In backgound, such mapping is added to special attribute app.url_map
 
-@app.route("/date")
-def date():
-    return "This page was served at " + str(datetime.now())
-
-counter = 0
-
-@app.route("/count_views")
-def count_demo():
-    global counter
-    counter += 1
-    return "This page was served " + str(counter) + " times"
+@app.route("/card")
+def card_view():
+    card = db[0]
+    return render_template(
+        "card.html",
+        card=card
+    )
